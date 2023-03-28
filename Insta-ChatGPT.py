537a43798729e3c748d1b97ce0cbdc1e569bcfdc
Dorian Milhau <85699054+Nairod-007@users.noreply.github.com>
2023-03-28 23:06:24 +0200

    Update Insta-ChatGPT.py

--- Insta-ChatGPT.py
+++ Insta-ChatGPT.py
@@ -6,16 +6,12 @@ from keyboard import read_event
 # -------- Configuration --------
 # Instagram
 cl = Client()
-cl.login('noxotev207', "aqwxcv")
+cl.login('id_account', "mdp_account")
 # ChatGPT
-mon_access_token = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6Ik1UaEVOVUpHTkVNMVFURTRNMEZCTWpkQ05UZzVNRFUxUlRVd1FVSkRNRU13UmtGRVFrRXpSZyJ9.eyJodHRwczovL2FwaS5vcGVuYWkuY29tL3Byb2ZpbGUiOnsiZW1haWwiOiJkb3JpYW5taWxoYXU3NTNAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOnRydWV9LCJodHRwczovL2FwaS5vcGVuYWkuY29tL2F1dGgiOnsidXNlcl9pZCI6InVzZXItZXoyb3JGdUU5WWpPZUoxR1p1bXRrczZ3In0sImlzcyI6Imh0dHBzOi8vYXV0aDAub3BlbmFpLmNvbS8iLCJzdWIiOiJnb29nbGUtb2F1dGgyfDExNTMyNDI4MjQ0MjgzNzY0ODAwMCIsImF1ZCI6WyJodHRwczovL2FwaS5vcGVuYWkuY29tL3YxIiwiaHR0cHM6Ly9vcGVuYWkub3BlbmFpLmF1dGgwYXBwLmNvbS91c2VyaW5mbyJdLCJpYXQiOjE2Nzg1NDQzODEsImV4cCI6MTY3OTc1Mzk4MSwiYXpwIjoiVGRKSWNiZTE2V29USHROOTVueXl3aDVFNHlPbzZJdEciLCJzY29wZSI6Im9wZW5pZCBwcm9maWxlIGVtYWlsIG1vZGVsLnJlYWQgbW9kZWwucmVxdWVzdCBvcmdhbml6YXRpb24ucmVhZCBvZmZsaW5lX2FjY2VzcyJ9.KUfCxlh3l5DkayoYfv8D0PFI7WkGXRNv3HsHSZS9IxCdDSbx82MknMHg81vCiv-T9LjSoyD86b0hwsaa62dOJ-2s4WiFBMJNosib-KkwInir7BNgzSRN3iGrbwLuZNhkckrYY3qhbbGrDeB-ZSM-Q0pNgvg8y2MM7DD92iocYKaCmxq8H7peULptuu5En0LSy3vOF9yHg4CZeiXR9ghpPzgh7mP7QCbXXL5TZ8Io1Yz07B6oZ98j3Cmp0BnpQDBeShIjGTEsrnKM5o34AN0Km19z05l7rNHFn95AXSgKj0X21K4J-r9-reDc0RZlCkfM6-qrXHeuHSfEILG373dIMg"
+mon_access_token = "access_token"
 chatbot = Chatbot(config={"access_token": mon_access_token})
 print("Connecter !")
 
-
-my_user_id = cl.user_id_from_username("noxotev207")
-print("Mon user_id :",my_user_id)
-print()
 
 # -------- (get) Message Insta (send) -> ChatGPT (get for send)-> Message Insta --------
 
